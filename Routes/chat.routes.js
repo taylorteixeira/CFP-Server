b542eaf5a08f4ad275a06d2b5828efcc2de0e938
Taylor Vargas Teixeira <taylorvargasteixeira@gmail.com>
2024-09-29 09:06:12 -0300

    Update chat.routes.js

--- Routes/chat.routes.js
+++ Routes/chat.routes.js
@@ -2,6 +2,6 @@ import express from "express"
 import chatController from "../Controllers/chat.controller.js" 
 
 const router = express.Router()
-router.post("/", chatController.sendMessageToAI)
+router.post("/send", chatController.sendMessageToAI)
 
 export default router
